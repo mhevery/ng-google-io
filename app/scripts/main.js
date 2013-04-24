cdaf45191bfbd868535d68121407b75cacd0380d
Misko Hevery <misko@hevery.com>
2013-04-23 23:19:28 -0700

    step2: AngularJS implementation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,113 +1,42 @@
 'use strict';
 
-var $container = $('.hero-unit');
+var projectsApp = angular.module('projects', ['ngResource']);
 
-var $projectsTABLE = $container.find('.list').remove();
-var $projectsTBODY = $projectsTABLE.find('tbody');
-var $projectTemplateTR = $projectsTBODY.find('>tr').remove();
-var $projectAdd = $projectsTABLE.find('thead>tr>th>a');
+projectsApp.config(function($routeProvider) {
+  $routeProvider.
+      when('/', {
+        controller: 'ProjectListCtrl as list',
+        templateUrl: 'ProjectList.html'
+      }).
+      when('/project/:id', {
+        controller: 'ProjectDetailCtrl as detail',
+        templateUrl: 'ProjectDetail.html'
+      });
+});
 
-var $detail = $container.find('form').remove();
-var $projectName = $detail.find(':input[name=name]');
-var $projectSite = $detail.find(':input[name=site]');
-var $projectDescription = $detail.find(':input[name=description]');
-var $detailCancel = $detail.find('a.btn');
-var $detailSave = $detail.find('.btn.btn-primary');
-var $detailDelete = $detail.find('.btn.btn-danger');
+projectsApp.factory('Project', function($resource) {
+  return $resource('/project/:id', { id: '@id' });
+});
 
-function ProjectListController() {
-  var self = this;
+projectsApp.controller('ProjectListCtrl', function(Project) {
+  this.projects = Project.query();
+});
 
-  this.show = function () {
-    $container.children().remove();
-    $container.append($projectsTABLE);
-    $projectsTBODY.find('>tr').remove();
-    $projectAdd.click(function(e) {
-      e.preventDefault();
-      detailController.show();
-    });
+projectsApp.controller('ProjectDetailCtrl', function(Project, $routeParams, $location) {
+  this.project = $routeParams.id ?
+      Project.get({id: $routeParams.id}) :
+      new Project();
 
-    $.ajax('/project').
-        done(function(projects) {
-          for(var i = 0, ii = projects.length; i < ii; i++) {
-            self.addProject(projects[i]);
-          }
-        });
+  this.save = function() {
+    this.project.$save(goHome);
   };
 
-  this.addProject = function (project) {
-    var row = $projectTemplateTR.clone();
-    row.find('>td:nth-child(1)>a').
-        text(project.name).
-        attr('href', project.url);
-    row.find('>td:nth-child(2)').text(project.description);
-    row.find('>td:nth-child(3)>a>i').
-        click(function(e) {
-          e.preventDefault();
-          detailController.show(project.id);
-        });
-    $projectsTBODY.append(row);
-  };
-}
-
-
-function ProjectDetailController() {
-  var self = this;
-
-  this.show = function(id) {
-    $container.children().remove();
-    $detailCancel.click(listController.show);
-    $detailSave.click(function(e) {
-      e.preventDefault();
-      self.saveDetail(id);
-    });
-    $detailDelete.click(function(e) {
-      e.preventDefault();
-      self.deleteDetail(id);
-    });
-
-    if (id) {
-      $.ajax('/project/' + id).
-          done(function(project) {
-            $projectName.val(project.name);
-            $projectSite.val(project.url);
-            $projectDescription.val(project.description);
-            $container.append($detail);
-          });
-    } else {
-      $projectName.val('');
-      $projectSite.val('');
-      $projectDescription.val('');
-      $container.append($detail);
-    }
+  this.delete = function() {
+    this.project.$delete(goHome);
   };
 
-  this.saveDetail = function(id) {
-    $.ajax({
-      url: '/project/' + (id || ''),
-      type: 'POST',
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
-        name: $projectName.val(),
-        description: $projectDescription.val(),
-        url: $projectSite.val()
-      })
-    }).done(listController.show);
-  };
+  function goHome() {
+    $location.path('/');
+  }
+});
 
-  this.deleteDetail = function(id) {
-    if (id) {
-      $.ajax({
-        url: '/project/' + id,
-        type: 'DELETE'
-      }).done(listController.show);
-    }
-  };
-}
-
-
-var listController = new ProjectListController();
-var detailController = new ProjectDetailController();
-
-listController.show();
